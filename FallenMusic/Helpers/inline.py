3b116ce2df8e15a6c1866e2cfe796c2355de3111
fast-malid <157091456+fast-malid@users.noreply.github.com>
2024-02-07 00:21:15 +0200

    Update inline.py

--- FallenMusic/Helpers/inline.py
+++ FallenMusic/Helpers/inline.py
@@ -33,7 +33,7 @@ close_key = InlineKeyboardMarkup(
 buttons = InlineKeyboardMarkup(
     [
         [
-            InlineKeyboardButton(text="▶️", callback_data="resume_cb"),
+            InlineKeyboardButton(text="▶", callback_data="resume_cb"),
             InlineKeyboardButton(text="⏸", callback_data="pause_cb"),
             InlineKeyboardButton(text="⏭️", callback_data="skip_cb"),
             InlineKeyboardButton(text="⏹", callback_data="end_cb"),
